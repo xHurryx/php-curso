2bdedda6874b21bf184e4f8043cbe5a1a1e34583
xHurryx <paulocd4@hotmail.com>
2018-11-20 12:09:40 -0200

    operadores logicos

---
+++ operadores-matematicos.php
@@ -0,0 +1,45 @@
+<?php 
+$var1 = 10;
+$var2 = 20;
+$var3 = 30;
+
+//adicao
+//echo $var1 + $var2; 
+echo "<br>";
+//subtracao
+//echo $var1 - $var2; 
+echo "<br>";
+//multiplicacao
+//echo $var1 * $var2; 
+echo "<br>";
+//divisao
+//echo $var1 / $var2; 
+echo "<br>";
+//modolo
+/*echo $var1 % 3; 
+echo "<br>";
+echo $var2 % 3; 
+echo "<br>";
+echo $var3 % 3; 
+echo "<br>";*/
+
+/*
+* ordem das operações
+echo $var1 + $var2 * $var3;
+echo "<br>";
+echo ($var1 + $var2) * $var3;*/
+
+// incremtos e decrementos
+/*$var1 = $var1 + 1;
+$var1++;
+$var1--;
+echo $var1;*/
+
+$var1 = $var1 + 10;
+$var1 += 10;
+echo $var1;
+echo "<br>";
+$aviso = '';
+$aviso .= 'Aminha messagem de aviso';
+echo $aviso;
+echo "<br>";
