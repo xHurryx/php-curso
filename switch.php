f4a87d2eb3aae9b7f1ba5d0e30fad839b9b233c6
xHurryx <paulocd4@hotmail.com>
2018-11-20 15:05:26 -0200

    switch

---
+++ switch.php
@@ -0,0 +1,26 @@
+<?php 
+$var = 1;
+
+switch ($var) {
+	case '1':
+		echo 'Variavel com valor 1';
+		case 'a';
+		break;
+	case '2':
+		echo 'Variavel com valor 2';
+		case 'b';
+		break;
+	case '3':
+		echo 'Variavel com valor 3';
+		case 'c';
+		break;
+	case '4':
+		echo 'Variavel com valor 4';
+		case 'd';
+		break;
+	
+	default:
+		echo 'Eu nao sei o valor da variavel';
+		break;
+}
+
