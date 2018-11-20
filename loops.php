31dc003c6043f790d59988ab869945a361e6a8eb
xHurryx <paulocd4@hotmail.com>
2018-11-20 15:34:26 -0200

    while

---
+++ loops.php
@@ -0,0 +1,36 @@
+<?php 
+
+for ($i=0; $i < 10; $i++) { 
+	echo 'i é igual a ' . $i . '<br>' ;
+}
+
+echo '<br><hr><br>';
+
+for ($variavel=0; $variavel < 10; $variavel++) { 
+	echo 'i é igual a ' . $variavel . '<br>' ;
+}
+
+echo '<br><hr><br>';
+echo 'for<br>';
+
+for ($i=10; $i > 0; $i--) { 
+	echo 'i é igual a ' . $i . '<br>' ;
+}
+
+echo '<br><hr><br>';
+echo 'while<br>';
+$i = 10;
+while ($i<10) { 
+	echo 'i é igual a ' . $i . '<br>' ;
+	$i++;
+}
+
+echo '<br><hr><br>';
+echo 'do while<br>';
+
+$i = -10;
+do{
+	echo 'i é igual a ' . $i . '<br>' ;
+	$i++;
+
+}while ($i < 10); 
