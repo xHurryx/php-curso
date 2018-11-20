3775519509b86af647dfc958e5d656681cbca9c8
xHurryx <paulocd4@hotmail.com>
2018-11-19 22:54:14 -0200

    tipos de comentarios

---
+++ comentarios.php
@@ -0,0 +1,25 @@
+<?php 
+// comentarios de uma linha 
+// 
+
+/*
+*comentarios de mais 
+*de uma 
+*linha
+*/
+
+/*
+comentarios de mais 
+de uma 
+linha
+*/
+
+/**
+ * @param type $var1
+ * @param type $var2
+ * @return boolean
+ * */
+
+function nome_da_funcao($var1, $var2){
+	return true;
+}
