4ff70394eb061d35a02119204d2f9b6c788bd1bc
xHurryx <paulocd4@hotmail.com>
2018-11-21 13:47:09 -0200

    formulario contato final

--- contato.php
+++ contato.php
@@ -1,41 +1,37 @@
 <?php 
-$mensagem = '';
-if (empty($_POST['nome'])){
-	$mensagem .= 'Favor preencher o nome  <br />';
-}else{
-	if (empty($_POST['email'])){
-	$mensagem .= 'Favor preencher o email  <br />';
-	}else{
-		if (empty($_POST['area'])){
-			$mensagem .= 'Favor preencher a area  <br />';
-		}else{
-			if (empty($_POST['mensagem'])){
-				$mensagem .= 'Favor preencher a mensagem  <br />';
-			}else{
-				$mensagem .='Obrigado por entrar en contato conosco!!<br />';
-				$mensagem .= $_POST['nome'] .'<br />';
-				$mensagem .= $_POST['email'] .'<br />';
-				$mensagem .= $_POST['area'] .'<br />';
-				$mensagem .= $_POST['mensagem']. '<br />';
-				$mensagem .= '<a href="formulario.html">Voltar<a/>';
-			}
-		}
-	}
-}
 
-/*if(!empty($_POST['nome']) && !empty($_POST['email']) && !empty($_POST['area']) && !empty($_POST['mensagem'])){
-	$mensagem = '';
-	$mensagem .='Obrigado por entrar en contato conosco!!
-	<br />';
-	$mensagem .= $_POST['nome'] .'<br />';
-	$mensagem .= $_POST['email'] .'<br />';
-	$mensagem .= $_POST['area'] .'<br />';
-	$mensagem .= $_POST['mensagem']. '<br />';
-	$mensagem .= '<a href="formulario.html">Voltar<a/>';
-}else{
-	$mensagem = 'Favor prencher coretamente o formulario<br /> <br />';
-	$mensagem .= '<a href="formulario.html">Voltar<a/>';
-} */
+if(isset($_POST['nome'])){	
+	$aviso = '';
+	$nome = $_POST['nome'];
+	$email = $_POST['email'];
+	$area = $_POST['area'];
+	$mensagem = $_POST['mensagem'];
+	
+	if (empty($nome)) {
+		$aviso .= 'Favor preencher o nome <br />';
+	}
+	if (empty($email)) {
+		$aviso .= 'Favor preencher o email <br />';
+	}
+	if (empty($area)) {
+		$aviso .= 'Favor preencher o area <br />';
+	}
+	if (empty($mensagem)) {
+		$aviso .= 'Favor preencher o mensagem <br />';
+	}
+	if (empty($aviso)) {
+		$aviso .= 'Recebemos sua mensagem. Dados enviados <br />';
+		$aviso .= 'Nome: ' . $nome . '<br />';
+		$aviso .= 'Nome: ' . $email . '<br />';
+		$aviso .= 'Nome: ' . $mensagem . '<br />';
+
+		$aviso .= '<br /><br /><a href="contato.php">voltar</a>'
+
+		$sucesso = true;
+	}	
+}/*else{
+	$aviso .= 'Formulario não foi enviado';
+}*/ 
 ?>
 
 <!DOCTYPE html>
@@ -45,8 +41,24 @@ if (empty($_POST['nome'])){
 	<title>Contato</title>
 </head>
 <body>
-	
-	<?php echo $mensagem; ?>
+	<?php if (!empty($aviso)): ?> 		
+		<h2><?php print $aviso; ?></h2>
+	<?php endif; ?>
+	<?php if (!$sucesso):?>
+	<form action="contato.php" method="post">
+		<label for="nome">Nome</label><input type="text" name="nome" id="nome" value="<?php if(isset($nome)){echo $nome;}?>"/><br />		
+		<label for="email">Email</label><input type="text" name="email" id="email" value="<?php if(isset($email)){echo $email;}?>"/><br />
+		Selecione para quem quer enviar:
+		<select name="area">
+			<option value="vendas">Vendas</option>
+			<option value="suporte">Suporte</option>
+		</select><br />
+		MENSAGEN<br />		
+		<textarea name="mensagem" cols="40" rows="10"/><?php if(isset($mensagem)){echo $mensagem;}?></textarea>
+		<br />
+		<input type="submit" value="Enviar">		
+	</form>
+	<?php endif; ?>
 
 </body>
 </html>
